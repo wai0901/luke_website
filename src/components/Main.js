2af0113ea6a60f8149ad41488e295fd7e11c6702
Kent leung <snail@hotmail.com>
2020-03-28 12:46:14 -0700

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from "react-router-dom";
+
+class Main extends Component {
+
+
+    render () {
+        return (
+            <h1>Hello</h1>
+        )
+    }
+}
+
+export default Main;
